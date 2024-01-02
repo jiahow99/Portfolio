40552f6d158494ab032972aca2c0683d459e535d
Kah How <jia_how99@hotmail.com>
2024-01-02 21:25:08 +0800

    Changed aos offset 100

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import 'aos/dist/aos.css';
 export default function Home() {
   useEffect(() => {
     AOS.init({
-      offset: 300,
+      offset: 100,
       duration: 600, // values from 0 to 3000, with step 50ms
       easing: 'ease', // default easing for AOS animations
       once: true, // whether animation should happen only once - while scrolling down
